e23365ba50791beafa26dfef5b746401fbf28047
Hyconin <90343138+Hycocin@users.noreply.github.com>
2023-03-31 00:35:25 +0300

    Delete Scripts.js

--- Scripts.js
+++
@@ -1,4 +0,0 @@
-let fonts = '';
-fonts = prompt('Вітаємо Вас на нашому сайті, вкажіть бажаний шрифт для сайту','Times New Roman')
-document.body.style.fontFamily = fonts
-document.title = document.title + ' ' + Date()
